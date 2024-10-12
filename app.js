f8601ad2e20f63791a01655cb69a1d11652f1fa2
Bharti Verma <bhartivermak2012@gmail.com>
2024-10-12 14:34:11 +0530

    Fetch all documents

--- app.js
+++ app.js
@@ -14,9 +14,31 @@ connectToDB((err) => {
     }
 })
 
-
-
 // routes 
 app.get('/greet', (req,res) => {
     res.send("Hello");
+})
+
+app.get("/books",(req,res) => {
+    // Using Find() which returns a cursor object to traverse through. we can use toArray or forEach methods to iterate and collect data
+    // using forEach
+    const books =[];
+    
+    db.collection('books')
+        .find()
+        .sort({ author : 1})
+        .forEach((book) => books.push(book))
+        .then(() => {
+            res.status(200).json(books);
+        })
+        .catch((err) => {
+            res.status(500).json({'error': "Could not fetch the documents !"})
+        })
+
+    // Below code explains how to use toArray method in node
+    //  db.collection("books").find().toArray().then((result) => {
+    //      console.log(result)
+    //      res.send({'books':result})
+    //  });
+    // res.send({books});
 })
